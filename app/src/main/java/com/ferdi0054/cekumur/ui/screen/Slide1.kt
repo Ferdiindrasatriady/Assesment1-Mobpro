e018dd4da9dc330189f92c5e34e196a6213d73d9
Ferdi Indra Satriady <ferdiindrasatriady3@gmail.com>
2025-04-09 22:05:59 +0700

    menambahkan text Apakah Anda siap untuk mengecek umur Anda

--- app/src/main/java/com/ferdi0054/cekumur/ui/screen/Slide1.kt
+++ app/src/main/java/com/ferdi0054/cekumur/ui/screen/Slide1.kt
@@ -6,12 +6,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ferdi0054.cekumur.R
 
 @Composable
@@ -30,7 +33,12 @@ fun Slide1(onLanjut: () -> Unit) {
             contentDescription = "Ilustrasi Cek Umur",
             modifier = Modifier.size(250.dp)
         )
-
+        Text(
+            text = "Apakah Anda siap untuk mengecek umur Anda?",
+            fontSize = 20.sp,
+            modifier = Modifier.padding(top = 24.dp),
+            style = MaterialTheme.typography.bodyLarge
+        )
 
 
     }
