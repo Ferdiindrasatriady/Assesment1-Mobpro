72f9a420542612274a80946a4dbf47f48829216f
Ferdi Indra Satriady <ferdiindrasatriady3@gmail.com>
2025-04-11 00:36:00 +0700

    membuat intro slide1 jadi di tengah

--- app/src/main/java/com/ferdi0054/cekumur/ui/screen/Slide1.kt
+++ app/src/main/java/com/ferdi0054/cekumur/ui/screen/Slide1.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,6 +37,7 @@ fun Slide1(navController: NavHostController) {
             text = stringResource(R.string.intro_s1),
             fontSize = 20.sp,
             modifier = Modifier.padding(top = 24.dp),
+            textAlign = TextAlign.Center,
             style = MaterialTheme.typography.bodyLarge
         )
         Button(
