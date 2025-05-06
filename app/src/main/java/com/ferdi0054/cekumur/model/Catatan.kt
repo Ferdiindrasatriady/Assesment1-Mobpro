c8f9920fcfb4e2cf213683b50c69100f586a5bed
Ferdi Indra Satriady <ferdiindrasatriady3@gmail.com>
2025-05-06 19:37:28 +0700

    Menyiapkan data di ViewMode

---
+++ app/src/main/java/com/ferdi0054/cekumur/model/Catatan.kt
@@ -0,0 +1,8 @@
+package com.ferdi0054.cekumur.model
+
+data class Catatan(
+    val id: Long,
+    val nama: String,
+    val tgl_lahir: String,
+    val tgl_skrg: String
+)
---
+++ app/src/main/java/com/ferdi0054/cekumur/ui/screen/MainViewModel.kt
@@ -0,0 +1,44 @@
+package com.ferdi0054.cekumur.ui.screen
+
+import com.ferdi0054.cekumur.model.Catatan
+
+class MainViewModel {
+    val data = listOf(
+        Catatan (
+            1,
+            "Ferdi",
+            "27-08-2005",
+            "06-05-2025"
+        ),
+        Catatan (
+            1,
+            "Ferdi",
+            "27-08-2005",
+            "06-05-2025"
+        ),
+        Catatan (
+            1,
+            "Ferdi",
+            "27-08-2005",
+            "06-05-2025"
+        ),
+        Catatan (
+            1,
+            "Ferdi",
+            "27-08-2005",
+            "06-05-2025"
+        ),
+        Catatan (
+            1,
+            "Ferdi",
+            "27-08-2005",
+            "06-05-2025"
+        ),
+        Catatan (
+            1,
+            "Ferdi",
+            "27-08-2005",
+            "06-05-2025"
+        )
+    )
+}
