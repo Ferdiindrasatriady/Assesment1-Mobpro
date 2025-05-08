95eb9cf74c5136ecb646de489a301e05ccd72fc4
Ferdi Indra Satriady <ferdiindrasatriady3@gmail.com>
2025-05-08 10:00:18 +0700

    membenarkan typo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.room.roomRunTime)
+    implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.room.compiler)
     implementation(libs.androidx.lifecyle.viewmodel.compose)
