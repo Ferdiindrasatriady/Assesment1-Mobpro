33f4739f2095d8011cf914f7e8908b717f690d27
Ferdi Indra Satriady <ferdiindrasatriady3@gmail.com>
2025-05-08 12:01:30 +0700

    benerin eror di gradle karena salah penulisan

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
-    implementation(libs.androidx.room.compiler)
+    ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.lifecyle.viewmodel.compose)
 
     testImplementation(libs.junit)
